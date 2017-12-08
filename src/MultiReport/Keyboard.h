63880a39d19a52a2cf291aeb0a75cc7fca7ed9c9
Gergely Nagy <algernon@keyboard.io>
2017-12-08 22:12:54 +0100

    WIP
    This is ungodly. Don't look.
    
    Signed-off-by: Gergely Nagy <algernon@keyboard.io>

--- src/MultiReport/Keyboard.h
+++ src/MultiReport/Keyboard.h
@@ -32,7 +32,9 @@ THE SOFTWARE.
 #include "HIDTables.h"
 #include "HIDAliases.h"
 
-#include "BootKeyboard/BootKeyboardAPI.h"
+#include "DescriptorPrimitives.h"
+
+#include "BootKeyboard/BootKeyboard.h"
 
 #define KEY_BYTES 28
 
@@ -45,8 +47,12 @@ typedef union {
   uint8_t allkeys[1 + KEY_BYTES];
 } HID_KeyboardReport_Data_t;
 
+typedef enum {
+  BOOT_FALLBACK,
+  NKRO_ONLY,
+} keyboard_report_type_t;
 
-
+template <keyboard_report_type_t report_type = BOOT_FALLBACK>
 class Keyboard_ {
  public:
   Keyboard_(void);
@@ -65,10 +71,249 @@ class Keyboard_ {
 
   HID_KeyboardReport_Data_t keyReport;
   HID_KeyboardReport_Data_t lastKeyReport;
-
-  BootKeyboardAPI *bootKeyboard = NULL;
  private:
   int sendReportUnchecked(void);
 };
-extern Keyboard_ Keyboard;
 
+extern Keyboard_<NKRO_ONLY> Keyboard;
+
+///
+
+static const uint8_t _hidMultiReportDescriptorKeyboard[] PROGMEM = {
+  //  NKRO Keyboard
+  D_USAGE_PAGE, D_PAGE_GENERIC_DESKTOP,
+  D_USAGE, D_USAGE_KEYBOARD,
+  D_COLLECTION, D_APPLICATION,
+  D_REPORT_ID, HID_REPORTID_NKRO_KEYBOARD,
+  D_USAGE_PAGE, D_PAGE_KEYBOARD,
+
+
+  /* Key modifier byte */
+  D_USAGE_MINIMUM, HID_KEYBOARD_FIRST_MODIFIER,
+  D_USAGE_MAXIMUM, HID_KEYBOARD_LAST_MODIFIER,
+  D_LOGICAL_MINIMUM, 0x00,
+  D_LOGICAL_MAXIMUM, 0x01,
+  D_REPORT_SIZE, 0x01,
+  D_REPORT_COUNT, 0x08,
+  D_INPUT, (D_DATA|D_VARIABLE|D_ABSOLUTE),
+
+
+	/* 5 LEDs for num lock etc, 3 left for advanced, custom usage */
+  D_USAGE_PAGE, D_PAGE_LEDS,
+  D_USAGE_MINIMUM, 0x01,
+  D_USAGE_MAXIMUM, 0x08,
+  D_REPORT_COUNT, 0x08,
+  D_REPORT_SIZE, 0x01,
+  D_OUTPUT, (D_DATA | D_VARIABLE | D_ABSOLUTE),
+
+// USB Code not within 4-49 (0x4-0x31), 51-155 (0x33-0x9B), 157-164 (0x9D-0xA4),
+// 176-221 (0xB0-0xDD) or 224-231 (0xE0-0xE7) NKRO Mode
+  /* NKRO Keyboard */
+  D_USAGE_PAGE, D_PAGE_KEYBOARD,
+
+  // Padding 3 bits
+  // To skip HID_KEYBOARD_NON_US_POUND_AND_TILDE, which causes
+  // Linux to choke on our driver.
+  D_REPORT_SIZE, 0x04,
+  D_REPORT_COUNT, 0x01,
+  D_INPUT, (D_CONSTANT),
+
+
+  D_USAGE_MINIMUM, HID_KEYBOARD_A_AND_A,
+  D_USAGE_MAXIMUM, HID_KEYBOARD_BACKSLASH_AND_PIPE,
+  D_LOGICAL_MINIMUM, 0x00,
+  D_LOGICAL_MAXIMUM, 0x01,
+  D_REPORT_SIZE, 0x01,
+  D_REPORT_COUNT, (HID_KEYBOARD_BACKSLASH_AND_PIPE - HID_KEYBOARD_A_AND_A)+1,
+  D_INPUT, (D_DATA|D_VARIABLE|D_ABSOLUTE),
+
+  // Padding 1 bit.
+  // To skip HID_KEYBOARD_NON_US_POUND_AND_TILDE, which causes
+  // Linux to choke on our driver.
+  D_REPORT_SIZE, 0x01,
+  D_REPORT_COUNT, 0x01,
+  D_INPUT, (D_CONSTANT),
+
+
+  D_USAGE_MINIMUM, HID_KEYBOARD_SEMICOLON_AND_COLON,
+  D_USAGE_MAXIMUM, HID_KEYBOARD_CANCEL,
+  D_LOGICAL_MINIMUM, 0x00,
+  D_LOGICAL_MAXIMUM, 0x01,
+  D_REPORT_SIZE, 0x01,
+  D_REPORT_COUNT, (HID_KEYBOARD_CANCEL-HID_KEYBOARD_SEMICOLON_AND_COLON) +1,
+  D_INPUT, (D_DATA|D_VARIABLE|D_ABSOLUTE),
+
+
+  // Padding 1 bit.
+  // To skip HID_KEYBOARD_CLEAR, which causes
+  // Linux to choke on our driver.
+  D_REPORT_SIZE, 0x01,
+  D_REPORT_COUNT, 0x01,
+  D_INPUT, (D_CONSTANT),
+
+  D_USAGE_MINIMUM, HID_KEYBOARD_PRIOR,
+  D_USAGE_MAXIMUM, HID_KEYPAD_HEXADECIMAL,
+  D_LOGICAL_MINIMUM, 0x00,
+  D_LOGICAL_MAXIMUM, 0x01,
+  D_REPORT_SIZE, 0x01,
+  D_REPORT_COUNT, (HID_KEYPAD_HEXADECIMAL - HID_KEYBOARD_PRIOR)  +1,
+  D_INPUT, (D_DATA|D_VARIABLE|D_ABSOLUTE),
+
+
+  // Padding (w bits)
+  D_REPORT_SIZE, 0x02,
+  D_REPORT_COUNT, 0x01,
+  D_INPUT, (D_CONSTANT),
+
+  D_END_COLLECTION,
+
+};
+
+template<keyboard_report_type_t type>
+Keyboard_<type>::Keyboard_(void) {
+  static HIDSubDescriptor node(_hidMultiReportDescriptorKeyboard, sizeof(_hidMultiReportDescriptorKeyboard));
+  HID().AppendDescriptor(&node);
+}
+
+template<keyboard_report_type_t type>
+void Keyboard_<type>::begin(void) {
+  // Force API to send a clean report.
+  // This is important for and HID bridge where the receiver stays on,
+  // while the sender is resetted.
+  releaseAll();
+  sendReportUnchecked();
+
+  if (type == BOOT_FALLBACK)
+    BootKeyboard.begin();
+}
+
+template<keyboard_report_type_t type>
+void Keyboard_<type>::end(void) {
+  releaseAll();
+  sendReportUnchecked();
+
+  if (type == BOOT_FALLBACK)
+    BootKeyboard.end();
+}
+
+template<keyboard_report_type_t type>
+int Keyboard_<type>::sendReportUnchecked(void) {
+    return HID().SendReport(HID_REPORTID_NKRO_KEYBOARD, &keyReport, sizeof(keyReport));
+}
+
+template<keyboard_report_type_t type>
+int Keyboard_<type>::sendReport(void) {
+  if (type == BOOT_FALLBACK) {
+    if (BootKeyboard.getProtocol() == HID_BOOT_PROTOCOL )
+      return BootKeyboard.sendReport();
+  }
+
+  // If the last report is different than the current report, then we need to send a report.
+  // We guard sendReport like this so that calling code doesn't end up spamming the host with empty reports
+  // if sendReport is called in a tight loop.
+
+  if (memcmp(lastKeyReport.allkeys, keyReport.allkeys, sizeof(keyReport))) {
+    // if the two reports are different, send a report
+    int returnCode = sendReportUnchecked();
+    memcpy(lastKeyReport.allkeys, keyReport.allkeys, sizeof(keyReport));
+    return returnCode;
+  }
+  return -1;
+}
+
+/* Returns true if the modifer key passed in will be sent during this key report
+ * Returns false in all other cases
+ * */
+template<keyboard_report_type_t type>
+boolean Keyboard_<type>::isModifierActive(uint8_t k) {
+  if (type == BOOT_FALLBACK) {
+    if (BootKeyboard.getProtocol() == HID_BOOT_PROTOCOL)
+      return BootKeyboard.isModifierActive(k);
+  }
+
+  if (k >= HID_KEYBOARD_FIRST_MODIFIER && k <= HID_KEYBOARD_LAST_MODIFIER) {
+    k = k - HID_KEYBOARD_FIRST_MODIFIER;
+    return !!(keyReport.modifiers & (1 << k));
+  }
+  return false;
+}
+
+/* Returns true if the modifer key passed in was being sent during the previous key report
+ * Returns false in all other cases
+ * */
+template<keyboard_report_type_t type>
+boolean Keyboard_<type>::wasModifierActive(uint8_t k) {
+  if (type == BOOT_FALLBACK) {
+    if (BootKeyboard.getProtocol() == HID_BOOT_PROTOCOL)
+      return BootKeyboard.wasModifierActive(k);
+  }
+
+  if (k >= HID_KEYBOARD_FIRST_MODIFIER && k <= HID_KEYBOARD_LAST_MODIFIER) {
+    k = k - HID_KEYBOARD_FIRST_MODIFIER;
+    return !!(lastKeyReport.modifiers & (1 << k));
+  }
+  return false;
+}
+
+template<keyboard_report_type_t type>
+size_t Keyboard_<type>::press(uint8_t k) {
+  if (type == BOOT_FALLBACK) {
+    if (BootKeyboard.getProtocol() == HID_BOOT_PROTOCOL)
+      return BootKeyboard.press(k);
+  }
+
+  // If the key is in the range of 'printable' keys
+  if (k <= HID_LAST_KEY) {
+    uint8_t bit = 1 << (uint8_t(k) % 8);
+    keyReport.keys[k / 8] |= bit;
+    return 1;
+  }
+
+  // It's a modifier key
+  else if (k >= HID_KEYBOARD_FIRST_MODIFIER && k <= HID_KEYBOARD_LAST_MODIFIER) {
+    // Convert key into bitfield (0 - 7)
+    k = k - HID_KEYBOARD_FIRST_MODIFIER;
+    keyReport.modifiers |= (1 << k);
+    return 1;
+  }
+
+  // No empty/pressed key was found
+  return 0;
+}
+
+template<keyboard_report_type_t type>
+size_t Keyboard_<type>::release(uint8_t k) {
+  if (type == BOOT_FALLBACK) {
+    if (BootKeyboard.getProtocol() == HID_BOOT_PROTOCOL)
+      return BootKeyboard.release(k);
+  }
+
+  // If we're releasing a printable key
+  if (k <= HID_LAST_KEY) {
+    uint8_t bit = 1 << (k % 8);
+    keyReport.keys[k / 8] &= ~bit;
+    return 1;
+  }
+
+  // It's a modifier key
+  else if (k >= HID_KEYBOARD_FIRST_MODIFIER && k <= HID_KEYBOARD_LAST_MODIFIER) {
+    // Convert key into bitfield (0 - 7)
+    k = k - HID_KEYBOARD_FIRST_MODIFIER;
+    keyReport.modifiers &= ~(1 << k);
+    return 1;
+  }
+
+  // No empty/pressed key was found
+  return 0;
+}
+
+template<keyboard_report_type_t type>
+void Keyboard_<type>::releaseAll(void) {
+  if (type == BOOT_FALLBACK) {
+    if (BootKeyboard.getProtocol() == HID_BOOT_PROTOCOL)
+      return BootKeyboard.releaseAll();
+  }
+
+  // Release all keys
+  memset(&keyReport.allkeys, 0x00, sizeof(keyReport.allkeys));
+}
